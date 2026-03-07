a566cef4fe97e39dfa7232350e377f234566fbfe
SafrankovO <olegsafrankov1234@gmail.com>
2026-03-08 00:49:31 +0100

    alpha version

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ document.querySelectorAll('nav a').forEach(anchor => {
             .scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+
+const pdfFiles = [
+    "pdf/MCH.02.01.01.pdf",
+    "pdf/MCH.03.01.01.pdf",
+    "pdf/MCH.04.01.01.pdf",
+    "pdf/MCH.05.01.01.pdf",
+    "pdf/MCH.06.01.01.pdf"
+];
+
+let current = 0;
+const iframe = document.getElementById("pdf-frame");
+
+document.getElementById("prev").addEventListener("click", ()=>{
+    current = (current - 1 + pdfFiles.length) % pdfFiles.length;
+    iframe.src = pdfFiles[current];
+});
+
+document.getElementById("next").addEventListener("click", ()=>{
+    current = (current + 1) % pdfFiles.length;
+    iframe.src = pdfFiles[current];
+});
